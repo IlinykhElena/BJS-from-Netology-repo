e5e0143bb12f06bbebde4e3a36f9cd5404c141dd
ElenaIlinykh <bengaliki22@gmail.com>
2022-11-06 09:01:12 +0700

    solveEquation written

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  
+  const d = b ** 2 - 4 * a * c;
+  
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  }
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d) ) / (2 * a));
+    arr.push((-b - Math.sqrt(d) ) / (2 * a));
+  }
+
   // код для задачи №1 писать здесь
   return arr; // array
 }
