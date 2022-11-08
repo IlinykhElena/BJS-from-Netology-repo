74448eeaa5d0edcbe4396be345abd437973b34b4
ElenaIlinykh <bengaliki22@gmail.com>
2022-11-08 22:41:10 +0700

    Tasks added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,54 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  min = Infinity;
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i+1]) {
+      min = arr[i];
+    }
+    if (arr[i] > arr[i+1]) {
+      max = arr[i];
+    }
+    sum += arr[i]; 
+    avg = (sum / arr.length).toFixed(2);    
+  }
+  
+  return { min: min, max: max, avg: Number(avg) };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
+  for (let i = 0; i < arr.lenght; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.lenght; i++) {
+    if (this.worker(arr[i]) > this.worker(arr[i+1])) {
+      max = this.worker(arr[i]);
+    } 
+  }
+ 
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max, min;
+  for (let i = 0; i < arr.lenght; i++) {
+    if (arr[i] < arr [i+1]) {
+      min = arr[i];
+    }
+    if (arr[i] > arr[i+1]) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
+
