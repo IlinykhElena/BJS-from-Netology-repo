cdfeb1674257b383842c36a5387f19d685c6428b
ElenaIlinykh <bengaliki22@gmail.com>
2022-11-10 22:26:11 +0700

    better version of compareArraye added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,6 +8,14 @@ function compareArrays(arr1, arr2) {
   return result; // boolean
 }
 
+function compareArraysBetterVersion(arr1, arr2) {
+  let result;
+  (arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])) ? result = true : result = false;
+  
+  return result; // boolean
+}
+
+
 function advancedFilter(arr) {
   let resultArr = arr.filter(number => number > 0).filter(number => number % 3 ===0).map(number => number * 10);
 
