5385e338f2f2bff31d57adc2c3bc8b989ac0c98c
ElenaIlinykh <bengaliki22@gmail.com>
2022-11-10 16:42:52 +0700

    functions modified

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if(arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])) {
+    result = true;
+  } else {
+    result = false;
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 ===0).map(number => number * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+    return resultArr; // array
 }
