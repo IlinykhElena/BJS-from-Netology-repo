8c5551f39b34e1db70fdd4b7f4986d5d68e8114c
ElenaIlinykh <bengaliki22@gmail.com>
2022-11-15 23:13:31 +0700

    task for 4.object added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+// let studentA = new Student("Nina", "female", 45);
+// let studentB = new Student("Sergei", "male", 38);
+// let studentC = new Student("Hunter Baiden", "male", 20);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    let marks = [mark];
+    this.marks = marks;
+  // добавить первую оценку 
+    } else {
+      this.marks = [...this.marks, mark];      
+    }
+}
+
+Student.prototype.addMarks = function (...newMarks) {
+  if(this.marks === undefined){ 
+    let marks = [...newMarks];
+    this.marks = marks;
+    } else {
+      this.marks = [...this.marks, ...newMarks];      
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((sum, current) => sum + current, 0);
+  let average = sum / this.marks.length;
+  return average;  
+}
+
+Student.prototype.exclude = function(reason) {
+  if(this.average < 2.5) {
+    delete this.subject;
+    delete this.marks;
+    reason = 'law grades';
+    this.excluded = reason;
+  }
+}
+
+ 
